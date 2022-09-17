2cc1d53122bb6e974366f5d2300ac6fba04fab0c
JEFFERSONDPR <jeffersondpr@gmail.com>
2022-09-17 12:50:57 -0300

    Aula layout 2

---
+++ app/src/main/java/com/escola/aposentar/AposentarIdade.kt
@@ -0,0 +1,14 @@
+package com.escola.aposentar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AposentarIdade:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+}
